2886328e4230c892b953acaee09a27fb4e848143
vmendoza <vmendoza@ucvts.tec.nj.us>
2018-01-09 18:11:25 +0000

    Added FinalProject17.py

---
+++ FinalProject17.py
@@ -0,0 +1,76 @@
+#create Character class
+"""class Character(object):
+
+    def __init__(self):
+        ""new character""
+
+    def use_skill():
+"""
+
+#lists and dictionaries
+#responses = ["Sorry, I don't understand that.", "", "", "", "", "", "", "", "", "", ""]
+#inventory = {}
+
+#functions
+def start():
+    """ """
+    print("")
+
+def invalid():
+    """used to respond to invalid commands"""
+    print()
+
+
+
+title = """~~~~~~~~~~~~~~~~~~~~
+|   (game title)   |
+|     > PLAY <     |
+|  > HOW TO PLAY < |
+~~~~~~~~~~~~~~~~~~~~
+"""
+
+print(title)
+
+commands = """check <object> : prompts character to look at object
+use <object >: uses object
+examine <object> : provides description of object
+inventory : prints inventory
+commands : displays command list
+"""
+
+
+choice1 = input("> ")
+
+if choice1.lower() == "play":
+
+elif choice1.lower() == "how to play":
+    in
+else:
+
+
+
+
+
+#Room 1
+
+
+
+#Room 2
+
+
+
+#Room 3
+
+
+
+#Room 4
+
+
+
+#Room 5
+
+
+
+#Room 6
+
+
