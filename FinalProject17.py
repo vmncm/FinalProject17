3f41fa0e6ef4dd24e283f94c32876df82a68abb3
vmendoza <vmendoza@ucvts.tec.nj.us>
2018-01-18 05:55:52 +0000

    fixed title loop, added initial situation

--- FinalProject17.py
+++ FinalProject17.py
@@ -1,15 +1,6 @@
 #import modules
 import random, sys, time
 
-#create Character class
-"""class Character(object):
-
-    def __init__(self):
-        ""new character""
-
-    def use_skill():
-"""
-
 #lists and dictionaries
 responses = ["Sorry, I don't understand that.", "Excuse me?", "Huh?", "Dude, that's not an option.", "Can't do that, sorry.", "Yeah, I'll go right ahead and do that for you! Just kidding.", "No.", "You're not the boss of me. Ugh.", "Can you...just maybe...make some sense?", "Oops! Sorry, I don't know how to do that!", "Maybe I can do that in the next update."]
 #inventory = {}
@@ -23,6 +14,20 @@ commands : displays command list
 how to play : shows instructions
 """
 
+title = """~~~~~~~~~~~~~~~~~~~~
+|      Escape      |
+|     > PLAY <     |
+|  > HOW TO PLAY < |
+~~~~~~~~~~~~~~~~~~~~
+"""
+
+instructions = """You can interact with objects using various preset commands.
+Objects that you can interact with are in all caps. To use commands with obects,
+type the command followed by the name of the object as it is shown in caps.
+
+Shown below are the various commands you can use throughout the game.
+
+"""
 
 #functions
 def type(str):
@@ -33,8 +38,10 @@ def type(str):
         time.sleep(0.05)
 
 def start():
-    """ """
-    print("")
+    """starts situation"""
+    type("...\n.....\nHow did you get here? No one's been here in centuries!")
+    new_name = input("Who are you?\n> ")
+    type()
 
 def invalid():
     """used to respond to invalid commands"""
@@ -52,42 +59,48 @@ def exa(object):
 
 def inv():
     """prints inventory"""
+    for k, v in inventory:
+        print(f"")
 
 def com():
     """displays command list"""
+    print(commands)
 
 def htp():
     """shows instructions"""
+    print(instructions)
     print(commands)
 
-title = """~~~~~~~~~~~~~~~~~~~~
-|   (game title)   |
-|     > PLAY <     |
-|  > HOW TO PLAY < |
-~~~~~~~~~~~~~~~~~~~~
+#create Character class
+"""class Character(object):
+
+    def __init__(self, name):
+        ""new character""
+        self.name = name
+
+    def use_skill():
+        ""use special skill""
 """
+
+
 
 print(title)
 
 
-
-
 choice1 = input("> ")
 
-#Not working
-if choice1.lower() == "play":
-    start()
-else:
-    while choice1.lower != "play":
-        if choice1.lower() == "how to play":
-            htp()
-            choice1 = input("> ")
-        else:
-            invalid()
-            choice1 = input("> ")
-    start()
+while True:
+    if choice1.lower() == "how to play":
+        htp()
+        choice1 = input("> ")
+    elif choice1.lower() == "play":
+        start()
+    else:
+        invalid()
+        choice1 = input("> ")
 
 
+#player = Character(new_name)
 
 
 #Room 1
